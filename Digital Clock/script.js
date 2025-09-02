407e270fcc3274f9dc00307890bb9abf5e4b4688
Aadi <sskumartarun@gmail.com>
2025-09-02 15:58:19 +0530

    some changes

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,6 +1,19 @@
 const clock = document.querySelector('#clock')
 
-setInterval(() => {
+const updateTime = () => {
     let date = new Date();
     clock.innerHTML = date.toLocaleTimeString();
-}, 1000);
+}
+updateTime();
+setInterval(updateTime, 1000);
+
+
+/*
+By calling updateTime() once before setInterval, the clock displays the current time immediately when the project loads. 
+If we only use setInterval, there would be a delay of 1 second before the time appears for the first time.
+*/
+
+// setInterval(() => {
+//     let date = new Date();
+//     clock.innerHTML = date.toLocaleTimeString();
+// }, 1000);
