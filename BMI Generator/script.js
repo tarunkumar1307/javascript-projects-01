2d4e009c801ec5ea6ca6fc4ff315fe42a73d3de2
Aadi <sskumartarun@gmail.com>
2025-09-02 15:47:27 +0530

    Digital Clock

--- BMI Generator/script.js
+++ BMI Generator/script.js
@@ -6,17 +6,25 @@ form.addEventListener('submit', function(e){
     const weight = parseInt(document.querySelector("#weight").value)
     const result = document.querySelector("#result")
     
-    if(height == '' && weight == ''){
-        result.textContent = `Please enter valid Height and Weight.`
-    }
-    else if(height == '' || height < 0 || isNaN(height)){
-        result.textContent = `Please enter a valid Height ${height}.`
+    if(height == '' || height < 0 || isNaN(height)){
+        result.textContent = `Please enter a valid Height, ${height}.`
     }
     else if(weight=='' || weight < 0 || isNaN(weight)){
-        result.textContent = `Please enter a valid Weight ${weight}.`
+        result.textContent = `Please enter a valid Weight, ${weight}.`
     }
     else{
         const ans = (weight / ((height*height)/10000)).toFixed(2);
-        result.textContent = `BMI = ${ans}`
+        if(ans < 18.6){
+            result.outerHTML = `BMI = ${ans} <br> Underweight</p>`;
+            result.style.color = "rgb(30, 174, 30)";
+        }
+        else if(ans >= 18.6 && ans < 24.9){
+            result.outerHTML = `BMI = ${ans} <br> Normal weight</p>`;
+            result.style.color = "rgb(30, 174, 30)";
+        }
+        else{
+            result.outerHTML = `BMI = ${ans} <br> Overweight</p>`;
+            result.style.color = "red";
+        }
     }
 })
